5ba1013fed88f595c878100e9c5aca534d25cafc
Somechandra Sen <somechandra.sen@CaprusIT.local>
2019-10-06 18:10:52 +0530

    Updates

---
+++ precache-manifest.69c950e99473d4273ef059268ed43171.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcc1136230a436fe74f434adea01a25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5f4065202aba2bb90397",
+    "url": "/monsters-rolodex/static/css/main.c1325508.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "5f4065202aba2bb90397",
+    "url": "/monsters-rolodex/static/js/main.d2fafa29.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
